ea78faad0ee5fa86603b58fbca78fd726769950d
trinhph1704 <trinhphse170414@fpt.edu.vn>
2025-12-14 16:02:06 +0700

    Fix

--- src/Router/CustomerRouter.tsx
+++ src/Router/CustomerRouter.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
 import { NotFound } from "../components";
-import AboutPage from "../features/about/pages/AboutPage";
 import {
   Blogs,
   BookingList,
@@ -11,25 +10,18 @@ import {
   EditProfile,
   Services
 } from "../features/customer";
-import BlogDetail from "../features/customer/pages/BlogDetails";
+
 import { BookingStatusPage } from "../features/customer/pages/BookingStatusPage";
 import CheckoutError from "../features/customer/pages/CheckoutError";
 import CheckoutRemainSuccess from "../features/customer/pages/CheckoutRemainSuccess";
 import CheckoutSuccess from "../features/customer/pages/CheckoutSuccess";
-import HomePage from "../features/home/pages/HomePage";
+
 
 export default function CustomerRouter() {
   return (
     <div className="flex-1">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/services/:id" element={<DetailServices />} />
-        {/* <Route path="/doctors" element={<Doctors />} /> */}
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogs/:id" element={<BlogDetail />} />
+        
         <Route path="/booking-status/:id" element={<BookingStatusPage />} />
         <Route path="/edit-booking/:id" element={<EditBooking />} />
         <Route path="/booking-list" element={<BookingList />} />
